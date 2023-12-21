cea9d0987066352212290acc8427f18728e41e9b
Steve Cherry <2136364+ok-steve@users.noreply.github.com>
2023-12-21 07:25:00 -0500

    Remove duplicate form event listener

--- public/script.js
+++ public/script.js
@@ -1,10 +1,3 @@
-// Disable normal form submission
-document
-  .querySelector('form:not([method]):not([action])')
-  .addEventListener('submit', (e) => {
-    e.preventDefault();
-  });
-
 // Sync input and output values
 document.querySelectorAll('input[type="range"][id]').forEach((input) => {
   input.addEventListener('input', (e) => {
