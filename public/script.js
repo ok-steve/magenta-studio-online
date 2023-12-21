893a3b385b4f1a67f115225e6607cebaf2981fff
ok-steve <2136364+ok-steve@users.noreply.github.com>
2023-12-21 06:28:52 -0500

    Remove normcore script

---
+++ public/script.js
@@ -0,0 +1,18 @@
+// Disable normal form submission
+document
+  .querySelector('form:not([method]):not([action])')
+  .addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
+
+// Sync input and output values
+document.querySelectorAll('input[type="range"][id]').forEach((input) => {
+  input.addEventListener('input', (e) => {
+    const id = e.target.id;
+    const output = document.querySelector(`output[for="${id}"]`);
+
+    if (output !== null) {
+      output.textContent = e.target.value;
+    }
+  });
+});
